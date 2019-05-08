e37f3d7e64828c8fa128a93d38f61b21e9f8b63f
dongtuoc <39508507+dongtuoc@users.noreply.github.com>
2019-05-08 11:02:09 +0800

    Update CRC.c

--- src/CRC.c
+++ src/CRC.c
@@ -6,21 +6,16 @@ U16 u16CRCVerify (const U8 *pu8Data,U32 u32Len,U16 u16CRCInit)
     U8  u8i=0;
     U32 u16CRC = u16CRCInit;
 
-    while (u32Len--) 
-	{
+    while (u32Len--) {
         u8i = 8;
         u16CRC = u16CRC ^ (((U16)*pu8Data++) << 8);
         do {
-            if (u16CRC & 0x8000) 
-			{
+            if (u16CRC & 0x8000) {
                 u16CRC = u16CRC << 1 ^ 0x1021;
-            }
-			else
-			{
+            }else{
                 u16CRC = u16CRC << 1;
             }
         } while (--u8i);
     }
     return u16CRC;
 }
-
