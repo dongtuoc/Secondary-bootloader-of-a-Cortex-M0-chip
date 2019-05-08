8fc7a61c59f9c53195d69b20b5f1bc93620839b7
dongtuoc <39508507+dongtuoc@users.noreply.github.com>
2019-05-08 11:08:39 +0800

    Update UART.c

--- src/UART.c
+++ src/UART.c
@@ -5,24 +5,24 @@
 
 void UART_Init (void)
 {
-    LPC_SWM->PINASSIGN[0] &= ~( 0xFFFFFF << 0 );
-    LPC_SWM->PINASSIGN[0] |=  ( 26 << 0 );                          
-    LPC_SWM->PINASSIGN[0] |=  ( 25 << 8 );                              
-    LPC_SWM->PINASSIGN[0] |=  ( 27 << 16);                            
-	
-    LPC_SYSCON->UARTCLKDIV     = 1;                                     
-    LPC_SYSCON->SYSAHBCLKCTRL |= (1<<14);                             
+  LPC_SWM->PINASSIGN[0] &= ~( 0xFFFFFF << 0 );
+  LPC_SWM->PINASSIGN[0] |=  ( 26 << 0 );                          
+  LPC_SWM->PINASSIGN[0] |=  ( 25 << 8 );                              
+  LPC_SWM->PINASSIGN[0] |=  ( 27 << 16);                            
+
+  LPC_SYSCON->UARTCLKDIV     = 1;                                     
+  LPC_SYSCON->SYSAHBCLKCTRL |= (1<<14);                             
+
+  LPC_USART0->BRG = SystemCoreClock * LPC_SYSCON->SYSAHBCLKDIV /
+                    (LPC_SYSCON->UARTCLKDIV * 16 * UART_BPS) - 1;   
     
-    LPC_USART0->BRG = SystemCoreClock * LPC_SYSCON->SYSAHBCLKDIV /
-                      (LPC_SYSCON->UARTCLKDIV * 16 * UART_BPS) - 1;   
-    
-    LPC_USART0->CFG = (1 << 0) |                                   
-                      (1 << 2) |                                       
-                      (2 << 4) |                                 
-                      (0 << 6) |                                        
-					  (1 << 18) |   //OETA
-					  (1 << 20) |    //OESEL
-					  (1 << 21);    //OEPOL
+  LPC_USART0->CFG = (1 << 0) |                                   
+                    (1 << 2) |                                       
+                    (2 << 4) |                                 
+                    (0 << 6) |                                        
+                    (1 << 18) |   //OETA
+                    (1 << 20) |    //OESEL
+                    (1 << 21);    //OEPOL
 }
 
 uint8_t UART_SendByte (uint8_t ucData)
